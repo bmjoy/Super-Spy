b5a657532f7d16d8c28ca0e21eed10844d1d7f95
laomd <965524991@qq.com>
2018-01-24 23:37:55 +0800

    游戏大厅

---
+++ Super Spy/Assets/Network/NetworkLobbyHook.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook : LobbyHook 
+{
+	public GameObject[] playerPrefabs;
+	int cnt;
+	void Start() {
+		cnt = 0;
+	}
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+		cnt = (cnt + 1) % playerPrefabs.Length;
+		GetComponent<LobbyManager> ().gamePlayerPrefab = playerPrefabs [cnt];
+    }
+}
