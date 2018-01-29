53cbb87b94e2b026fc61c59ea185a30fb8d12024
laomd <965524991@qq.com>
2018-01-29 19:30:08 +0800

    游戏退出

---
+++ Super Spy/Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	float last_esc_time = 0;
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (last_esc_time == 0 || Time.time - last_esc_time > 1.0f) {
+				last_esc_time = Time.time;
+			} else {
+				Application.Quit ();
+			}
+		}
+	}
+}
